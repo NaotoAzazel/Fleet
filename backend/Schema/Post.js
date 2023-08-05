af556ce9e462d2645ee6437a7b1284c23d974ea9
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2023-08-05 16:28:27 +0300

    add: "category" field

--- backend/Schema/Post.js
+++ backend/Schema/Post.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const Post = new mongoose.Schema({
   name: { type: String, required: true },
-  takeBy: { type: String },
+  takeBy: { type: String, required: true },
   color: { type: String, required: true },
   plate: { type: String, required: true },
+  category: { type: String, required: true },
   image: { type: String },
 }, { timestamps: true });
 
