d4e89d924772e78aa95ea2bb0e0d9d1c86d42505
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2023-10-13 20:43:55 +0300

    edited text on card

--- frontend/src/Components/cards/PostsNotFound.jsx
+++ frontend/src/Components/cards/PostsNotFound.jsx
@@ -2,8 +2,8 @@ import notepad from "../../assets/notepad.png";
 
 function PostsNotFound() {
   return (
-    <div className="mx-auto sm:p-6 grid place-items-center gap-5 p-3 bg-modalBackground rounded-[10px] 
-      border border-borderColor">
+    <div className="mx-auto sm:p-6 grid place-items-center gap-5 p-3 bg-modalBackground 
+      rounded-[10px]">
       <div className="overflow-hidden rounded-lg place-items-center">
         <img 
           src={notepad}
@@ -15,8 +15,8 @@ function PostsNotFound() {
       </div>
       <div className="grid gap-3">
         <h1 className="font-bold font-manrope sm:text-3xl 
-          tracking-tight mx-auto text-2xl">Не создано ни одного поста</h1>
-        <p className="text-muted-foreground mx-auto">Здесь пока нет постов</p>
+          tracking-tight mx-auto text-2xl">Не удалось найти посты</h1>
+        <p className="text-muted-foreground mx-auto">Здесь пока нет постов. Попробуйте заменить фильтры, чтобы получить новые результаты</p>
       </div>
     </div>
   )
