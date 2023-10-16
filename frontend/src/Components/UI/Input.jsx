2c72d768138445b075a68a4b16fb13da499ec643
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2023-10-16 13:16:24 +0300

    redesigned input

--- frontend/src/Components/UI/Input.jsx
+++ frontend/src/Components/UI/Input.jsx
@@ -3,8 +3,10 @@ function Input({ type, placeholder, styles, ...props }) {
     <>
       <input 
         type={type}
-        className={`bg-transparent p-2 rounded-[10px] border border-borderColor 
-          focus:input ${styles}`}
+        className={`bg-transparent p-2 rounded-md border border-input 
+          px-3 py-2 text-medium file:border-0 file:bg-transparent file:text-sm 
+          file:font-medium placeholder:text-muted-foreground focus-visible:outline-none 
+          disabled:cursor-not-allowed disabled:opacity-50 max-w-sm outline-none ${styles}`}
         placeholder={placeholder}
         {...props}
       />
