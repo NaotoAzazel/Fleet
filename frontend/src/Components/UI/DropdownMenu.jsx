381d4f60a9c13418d1f11f7515e06c096043d6d5
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2023-10-16 13:16:09 +0300

    destructurized components, improved code readability

--- frontend/src/Components/UI/DropdownMenu.jsx
+++ frontend/src/Components/UI/DropdownMenu.jsx
@@ -1,12 +1,85 @@
 import { Button } from "./Button.jsx";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, forwardRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp, faChevronDown, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
 
+export function ListItem({ name, value, selectValue, onSelectChange, setIsActive }) {
+  return (
+    <li
+      className={`py-2 px-4 pr-8 rounded text-white cursor-pointer text-sm hover:bg-accent 
+        ${selectValue === value && "font-bold"}`}
+      key={value}
+      onClick={() => {
+        onSelectChange(value);
+        setIsActive(false);
+      }}
+    >
+      {name}
+    </li>
+  )
+}
+
+export const ListContainer = forwardRef(({
+  filteredOptions, 
+  isActive, 
+  setIsActive, 
+  selectValue, 
+  onSelectChange,
+  children
+}, ref) => {
+  return (
+    <motion.ul 
+      className={`bg-background mt-1 z-10 p-0 shadow-lg rounded-[10px] absolute border 
+        border-borderColor ${!isActive && "hidden"} overflow-y-auto max-h-60`}
+      ref={ref}
+      animate={isActive ? { opacity: 1 } : { opacity: .5 }} 
+      transition={{ ease: "easeIn", duration: 0.2 }}
+    >
+      {children}
+      <div className="p-1">
+        {!filteredOptions.length && <ErrorRow title="Совпадений не найдено." />}
+        {filteredOptions.map(({ name, value }) => (
+          <ListItem 
+            name={name}
+            value={value}
+            selectValue={selectValue}
+            onSelectChange={onSelectChange}
+            setIsActive={setIsActive}
+          />
+        ))}
+      </div>
+    </motion.ul>
+  )
+});
+
+function ErrorRow({ title }) {
+  return (
+    <div className="py-6 text-center text-sm">
+      {title}
+    </div>
+  )
+}
+
+function SearchRow({ inputPlaceHolder, inputValue, setInputValue }) {
+  return (
+    <div className="flex items-center w-full border-b border-borderColor px-3">
+      <FontAwesomeIcon icon={faMagnifyingGlass} className="mr-2" size="sm"/>
+      <input 
+        className="w-40 h-11 bg-transparent py-3 disabled:cursor-not-allowed outline-none
+          placeholder:text-muted-foreground disabled:opacity-50"
+        placeholder={inputPlaceHolder}
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value.toLowerCase())}
+      />
+    </div>
+  )
+}
+
 const dropdownRefs = [];
 
-function DropdownMenu({ 
+export function DropdownMenu({ 
   title, 
   options, 
   selectValue, 
@@ -19,7 +92,7 @@ function DropdownMenu({
   const [inputValue, setInputValue] = useState("");
   const [filteredOptions, setFilteredOptions] = useState(options);
   const menuRef = useRef(null); 
-  
+
   dropdownRefs.push({ ref: menuRef, setIsActive: setIsActive });
 
   useEffect(() => {
@@ -29,9 +102,8 @@ function DropdownMenu({
 
   useEffect(() => {
     const handleDocumentClick = (event) => {
-      if (menuRef.current && !menuRef.current.contains(event.target)) {
+      if (menuRef.current && !menuRef.current.contains(event.target))
         setIsActive(false);
-      }
     };
 
     document.addEventListener("click", handleDocumentClick);
@@ -64,49 +136,22 @@ function DropdownMenu({
         <FontAwesomeIcon icon={isActive ? faChevronUp : faChevronDown} className="ml-2" />
       </Button>
 
-      <motion.ul  
-        className={`bg-background mt-1 z-10 p-0 shadow-lg rounded-[10px] absolute border 
-          border-borderColor ${!isActive && "hidden"} overflow-y-auto max-h-60`}
+      <ListContainer 
+        filteredOptions={filteredOptions} 
+        isActive={isActive} 
+        setIsActive={setIsActive} 
+        selectValue={selectValue}
+        onSelectChange={onSelectChange}
         ref={menuRef}
-        animate={isActive ? { opacity: 1 } : { opacity: .5 }} 
-        transition={{ ease: "easeIn", duration: 0.2 }}
       >
-      {isSearchable && (
-        <div className="flex items-center w-full border-b border-borderColor px-3">
-          <FontAwesomeIcon icon={faMagnifyingGlass} className="mr-2" size="sm"/>
-          <input 
-            className="w-40 h-11 bg-transparent py-3 disabled:cursor-not-allowed outline-none
-              placeholder:text-muted-foreground disabled:opacity-50"
-            placeholder={inputPlaceHolder}
-            type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value.toLowerCase())}
+        {isSearchable && (
+          <SearchRow 
+            inputPlaceHolder={inputPlaceHolder}
+            inputValue={inputValue}
+            setInputValue={setInputValue}
           />
-        </div>
-      )}
-      <div className="p-1">
-        {!filteredOptions.length && (
-          <div className="py-6 text-center text-sm">
-            Совпадений не найдено.
-          </div>
         )}
-        {filteredOptions.map(({ name, value }) => (
-          <li
-            className={`py-2 px-4 pr-8 rounded text-white cursor-pointer text-sm hover:bg-accent 
-              ${selectValue === value && "font-bold"}`}
-            key={value}
-            onClick={() => {
-              onSelectChange(value);
-              setIsActive(false);
-            }}
-          >
-            {name}
-          </li>
-        ))}
-      </div>
-      </motion.ul>
+      </ListContainer>
     </div>
   )
 }
-
-export default DropdownMenu;
