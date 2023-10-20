4018af002abc005d9ecfdc907745d3328b6d2fdd
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2023-10-20 20:07:17 +0300

    homepage redesign, skeleton card transferred to one file

--- frontend/src/Components/Pages/MainPage.jsx
+++ frontend/src/Components/Pages/MainPage.jsx
@@ -1,11 +1,51 @@
 import { Button } from '../UI/Button.jsx';
 import { Element } from "react-scroll";
 import InformationCard from '../cards/InformationCard.jsx';
+import { useFetching } from "../../hooks/useFetching.js"; 
+import PostService from '../../API/PostService.js';
+import { useState, useEffect, memo, useMemo } from 'react';
+import { SlicedProductCard, SkeletonCard } from '../cards/ProductCard.jsx';
+
+const MemorizedPosts = memo(SlicedProductCard);
+
+function SelectionContainer({ children }) {
+  return (
+    <selection className="mx-auto flex w-full max-w-[64rem] flex-col 
+      justify-center gap-4 py-12 md:pt-32 container space-y-2">
+      {children}
+    </selection>
+  )
+} 
 
 function MainPage() {
+  const [posts, setPosts] = useState([]);
+  const isFetchPostsCalled = useState(false);
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async(limit, page) => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+  });
+
+  useEffect(() => {
+    if (!isFetchPostsCalled?.value) {
+      fetchPosts(3, 1);
+      isFetchPostsCalled.value = true;
+    }
+  }, []);
+
+  const memorizedPosts = useMemo(() => {
+    return posts.map((post) => 
+      <MemorizedPosts 
+        key={post._id}
+        image={`data:image/png;base64,${post.image}`}
+        title={post.name}
+      />
+    ) 
+  });
+
   return (
     <main className="bg-background min-h-screen text-white relative flex flex-col items-center">
-      <selection className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+      <SelectionContainer>
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <h1 className="text-3xl tracking-normal font-bold font-manrope md:text-5xl lg:text-6xl">
             Приложение, построенное с использованием React 18
@@ -15,9 +55,9 @@ function MainPage() {
             <Button variant="outline" href="/asd">GitHub</Button>
           </div>
         </div>
-      </selection>
+      </SelectionContainer>
 
-      <selection className="container space-y-6 py-28 md:py-12 md:pt-24 lg:py-36">
+      <SelectionContainer>
         <Element name="technologies">
           <h2 className="text-center mx-auto max-w-[58rem] font-heading tracking-normal text-3xl md:text-4xl 
             lg:text-5xl font-bold font-manrope">
@@ -26,11 +66,6 @@ function MainPage() {
         </Element>
 
         <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
-          <InformationCard 
-            title="React 18" 
-            text="Приложения, маршрутизация, макеты, загрузка пользовательского интерфейса и API." 
-          />
-
           <InformationCard 
             title="Database"
             text="ORM с использованием MongoDB с Mongoose и развернутая на MongoDB Atlas."
@@ -46,7 +81,27 @@ function MainPage() {
             text="Аутентификация с использованием Supabase и middlewares."
           />
         </div>
-      </selection>
+      </SelectionContainer>
+
+      <SelectionContainer>
+        <h2 className="text-center mx-auto max-w-[58rem] font-heading tracking-normal text-3xl md:text-4xl 
+          lg:text-5xl font-bold font-manrope">
+          Доступный транспорт
+        </h2>
+        <div className="flex flex-col">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+            {isPostsLoading
+              ? <SkeletonCard cards={3}/>
+              : <>{memorizedPosts}</>
+            }
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          <Button href="/transport">
+            Список транспорта
+          </Button>
+        </div>
+      </SelectionContainer>
     </main>
   )
 }
--- frontend/src/Components/cards/ProductCard.jsx
+++ frontend/src/Components/cards/ProductCard.jsx
@@ -1,7 +1,62 @@
 import { Button } from "../UI/Button.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faImage } from "@fortawesome/free-solid-svg-icons";
 import "../../Styles/globals.css";
 
-function ProductCard({ image, title, buttonText }) {
+export function SkeletonCard({ cards }) {
+  return (
+    Array(cards).fill(0).map((item, index) => (
+      <div className="border border-borderColor overflow-hidden rounded-[10px]" key={index}>
+        <div className="flex flex-col border-borderColor p-0">
+          <div className="relative flex pb-48 inset-0">
+            <div className="absolute inset-0">
+              <div className="flex w-full h-full items-center justify-center bg-secondary">
+                <FontAwesomeIcon icon={faImage} className="w-[36px] h-[36px]" style={{color: "#a1a1aa"}}/>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-4">
+          <div className="bg-zinc-700 w-1/2 h-5 animate-pulse rounded-lg" />
+        </div>
+
+        <div className="flex items-center p-4">
+          <div className="flex w-full flex-col items-center gap-2 sm:flex-row sm:justify-between">
+            <div className="inline-flex h-9 px-2 rounded-md w-full bg-zinc-700 animate-pulse" />
+            <div className="inline-flex h-9 px-2 rounded-md w-full bg-zinc-700 animate-pulse" />
+          </div>
+        </div>
+      </div>
+    ))
+  )
+}
+
+export function SlicedProductCard({ image, title }) {
+  return (
+    <div className="border border-borderColor overflow-hidden rounded-[10px]">
+      <div className="flex flex-col border-borderColor p-0">
+        <div className="relative flex pb-48 inset-0">
+          <div className="absolute inset-0">
+            <div className="flex w-full h-full items-center justify-center bg-secondary">
+              <img 
+                src={image} 
+                className="absolute h-full w-full inset-0 border-borderColor object-cover"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                loading="lazy"
+              /> 
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-4">
+        <h1>{title}</h1>
+      </div>
+    </div>
+  )
+}
+
+export function ProductCard({ image, title, buttonText }) {
   return (
     <div className="border border-borderColor overflow-hidden rounded-[10px]">
       <div className="flex flex-col border-borderColor p-0">
@@ -32,5 +87,3 @@ function ProductCard({ image, title, buttonText }) {
     </div>
   )
 }
-
-export default ProductCard
