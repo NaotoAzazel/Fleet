5d0750c727ef99edd421887b3780bd2a5985455e
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2024-02-08 20:35:58 +0300

    added a website template

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter, Manrope } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const manrope = Manrope({ subsets: ["latin"], variable: "--font-heading" });
+
+export const metadata: Metadata = {
+  title: "Fleet",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <body className={cn("relative h-full font-sans antialiased", `${inter.variable} font-sans`, `${manrope.variable} font-sans`)}>
+        <main className="relative flex flex-col min-h-screen">
+          <div className="flex-grow flex-1">
+            {children}
+          </div>
+        </main>
+      </body>
+    </html>
+  );
+}
